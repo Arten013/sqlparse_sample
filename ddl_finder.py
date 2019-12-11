b0b2779780c3a00e308d5ae1963a8eb80234b20d
Arten013 <fukkn_kaz@yahoo.co.jp>
2019-12-12 00:59:07 +0900

    bugfix

--- ddl_finder.py
+++ ddl_finder.py
@@ -1,31 +1,37 @@
 from glob import iglob
-from typing import Optional, List, Sequence
+from typing import Optional, List, Iterable
 from sqlparse.tokens import Keyword, DDL, Token, Name, Punctuation
-from sqlparse.sql import Statement
 import sqlparse
-import os
+from pathlib import Path
 
 TokenType = Token.__class__
 DEFAULT_DB = "default"
 
-def find_hive_ddl(ddl_folder_path: str, table_name: str, db_name: Optional[str]=None):
+
+def find_hive_ddl(directory: str, table: str, db: Optional[str] = None):
     """引数で指定したテーブルの定義スクリプトをフォルダから探し出す関数"""
-    for p in iglob(ddl_folder_path, recursive=True):
-        if not os.path.isfile(p):
+    for p in iglob(directory, recursive=True):
+        path = Path(p)
+        if not path.is_file():
             continue
-        with open(p) as f:
-            if find_table_definition(f, table_name, db_name):
+        with p.open() as f:
+            if find_table_definition(f.read(), table, db):
                 return p
     raise FileNotFoundError(
-        "DDL file of {table_name} not found.".format(table_name=table_name)
+        "DDL file of {table_name} not found.".format(table_name=table)
     )
 
-def find_table_definition(sql: str, table_name: str, db_name: Optional[str]=None) -> Optional[TokenType]:
+
+def find_table_definition(
+        sql: str, table: str, db: Optional[str] = None) -> Optional[TokenType]:
     """`table_name`で指定したテーブルの定義を文字列`sql`中から見つける"""
-    db_name = db_name or DEFAULT_DB
+    db = db or DEFAULT_DB
     # 1. パースしてトークン列に分割
     for query in sqlparse.parse(sql):
-        tokens = [t for t in query.flatten() if not (t.is_whitespace or is_comment(t))]
+        tokens = [
+            t for t in query.flatten()
+            if not (t.is_whitespace or is_comment(t))
+        ]
         # 2. <DDL CREATE>を検証
         if not match_consume(tokens, (DDL, "CREATE")):
             continue
@@ -42,18 +48,20 @@ def find_table_definition(sql: str, table_name: str, db_name: Optional[str]=None
             if not match_consume(tokens, (Keyword, "EXISTS")):
                 continue
         # 6. テーブル名が一致したら返す。
-        if db_name == DEFAULT_DB and match_consume(tokens, (Name, table_name)):
+        if db == DEFAULT_DB and match_consume(tokens, (Name, table)):
             return query
-        if (match_consume(tokens, (Name, db_name))
+        if (match_consume(tokens, (Name, db))
                 and match_consume(tokens, (Punctuation, "."))
-                and match_consume(tokens, (Name, table_name))
-            ):
+                and match_consume(tokens, (Name, table))
+        ):
             return query
+
 
 def is_comment(token: TokenType) -> bool:
     return token.ttype in sqlparse.tokens.Comment
 
-def match_consume(tokens: List[TokenType], match_args: Sequence) -> bool:
+
+def match_consume(tokens: List[TokenType], match_args: Iterable) -> bool:
     if tokens[0].match(*match_args):
         tokens.pop(0)
         return True
